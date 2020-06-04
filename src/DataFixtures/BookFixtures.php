8ef33e6a4944689640ce7f51adf3f544dc9e650a
benaor <benjamingirard25@gmail.com>
2020-06-04 23:11:19 +0200

    install faker and send fixtures in database

--- src/DataFixtures/BookFixtures.php
+++ src/DataFixtures/BookFixtures.php
@@ -2,16 +2,31 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Book;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class BookFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
 
+        for ($i = 0; $i < 25; $i++) {
+
+            $book = new Book();
+            $book->setTitle($faker->sentence($nbWords = 3, $variableNbWords = true))
+                ->setIsbn(strval($faker->isbn10))
+                ->setDescription($faker->sentence($nbWords = 6, $variableNbWords = true))
+                ->setPrice($faker->numberBetween($min = 10, $max = 40))
+                ->setStock($faker->numberBetween($min = 1, $max = 30))
+                ->setYear($faker->numberBetween($min = 1940, $max = 2020))
+                ->setImage('https://picsum.photos/640/480');
+
+            $manager->persist($book);
+           
+        }
+        
         $manager->flush();
     }
 }
