b44f55430636dff0fa162cf4c95fc883eae17013
benaor <benjamingirard25@gmail.com>
2020-06-04 22:00:08 +0200

    view all book

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,11 @@ class ShopController extends AbstractController
     /**
      * @Route("/", name="shop-home")
      */
-    public function index()
+    public function index(BookRepository $bookRepository)
     {
         return $this->render('shop/shop.html.twig', [
             'page_title' => 'Boutique Vesoul-edition',
+            'books' => $bookRepository->findAll()
         ]);
     }
 }
