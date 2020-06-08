52557974ee88f110d41551f8ca5f4c9fca51684b
benaor <benjamingirard25@gmail.com>
2020-06-09 01:15:50 +0200

    create route for edit book value (in newBook)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,6 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -38,24 +41,99 @@ class AdminController extends AbstractController
 
     /**
      * @Route("/books/new", name="admin_new_book")
+     * @Route("/books/edit/{id}", name="admin_edit_book")
      */
-    public function newBook(Request $request, EntityManagerInterface $manager)
+    public function newBook(Book $book = null, Request $request, EntityManagerInterface $manager)
     {
-
-        $book = new Book();
+        if (!$book) {
+            $book = new Book();
+        }
+        else {
+            $book->setTitle('test');
+        }
 
         $form = $this->createFormBuilder($book)
-            ->add('title')
-            ->add('isbn')
-            ->add('description')
-            ->add('price')
-            ->add('stock')
-            ->add('year')
-            ->add('image')
+            ->add('title', TextType::class, [
+                'label' => 'Titre du livre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold py-1 m-0 col-4'
+                ],
+                'attr' => [
+                    'class' => 'my-1 col-7'
+                ]
+            ])
+            ->add('isbn', TextType::class, [
+                'label' => 'Code ISBN du livre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold py-1 m-0 col-4'
+                ],
+                'attr' => [
+                    'class' => 'my-1 col-7'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du livre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold py-1 m-0 col-4'
+                ],
+                'attr' => [
+                    'class' => 'my-1 col-7'
+                ]
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Prix du livre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold py-1 m-0 col-4'
+                ],
+                'attr' => [
+                    'class' => 'my-1 col-7'
+                ]
+            ])
+            ->add('stock', NumberType::class, [
+                'label' => 'Nombre de livre en stock',
+                'label_attr' => [
+                    'class' => 'font-weight-bold py-1 m-0 col-4'
+                ],
+                'attr' => [
+                    'class' => 'my-1 col-7'
+                ]
+            ])
+            ->add('year', NumberType::class, [
+                'label' => 'Date de sortie du livre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold py-1 m-0 col-4'
+                ],
+                'attr' => [
+                    'class' => 'my-1 col-7'
+                ]
+            ])
+            ->add('image', TextType::class, [
+                'label' => 'image du livre',
+                'label_attr' => [
+                    'class' => 'font-weight-bold py-1 m-0 col-4'
+                ],
+                'attr' => [
+                    'class' => 'my-1 col-7'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-secondary text-light'
+                ]
+            ])
             ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($book);
+            $manager->flush();
+            return $this->redirectToRoute('show_book', ['id' => $book->getId()]);
+        }
+
         return $this->render('admin/books/new.html.twig', [
-            'page_title' => 'Ajouter un livre dans la boutique',
+            'page_title' => 'Ajout/modification d\'un livre dans la boutique',
             'formNewBook' => $form->createView()
         ]);
     }
