8ec58bfbccdf45f4108fe0d68e94f5641446f150
benaor <benjamingirard25@gmail.com>
2020-06-08 23:46:25 +0200

    prototype of form for newBook

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -35,7 +38,20 @@ class AdminController extends AbstractController
     /**
      * @Route("/books/new", name="admin_new_book")
      */
-    public function newBook(){
+    public function newBook(Request $request, EntityManagerInterface $manager){
+        if ($request->request->count() > 0) {
+            $book = new Book();
+            $book->setTitle($request->request->get('title'))
+                ->setIsbn($request->request->get('isbn'))
+                ->setDescription($request->request->get('description'))
+                ->setPrice($request->request->get('price'))
+                ->setStock($request->request->get('stock'))
+                ->setYear($request->request->get('year'))
+                ->setImage($request->request->get('image'));
+
+            $manager->persist($book);
+            $manager->flush();
+        }
         return $this->render('admin/books/new.html.twig', [
             'page_title' => 'Ajouter un livre dans la boutique',
         ]);
