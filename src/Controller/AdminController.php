23af38d063f55f2f1167c9bc35211e46320ce68e
benaor <benjamingirard25@gmail.com>
2020-06-09 01:34:20 +0200

    move all form->add() in formCreateBuilder to BookType.php

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,14 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
@@ -43,86 +40,13 @@ class AdminController extends AbstractController
      * @Route("/books/new", name="admin_new_book")
      * @Route("/books/edit/{id}", name="admin_edit_book")
      */
-    public function newBook(Book $book = null, Request $request, EntityManagerInterface $manager)
+    public function formBook(Book $book = null, Request $request, EntityManagerInterface $manager)
     {
         if (!$book) {
             $book = new Book();
         }
-        else {
-            $book->setTitle('test');
-        }
 
-        $form = $this->createFormBuilder($book)
-            ->add('title', TextType::class, [
-                'label' => 'Titre du livre',
-                'label_attr' => [
-                    'class' => 'font-weight-bold py-1 m-0 col-4'
-                ],
-                'attr' => [
-                    'class' => 'my-1 col-7'
-                ]
-            ])
-            ->add('isbn', TextType::class, [
-                'label' => 'Code ISBN du livre',
-                'label_attr' => [
-                    'class' => 'font-weight-bold py-1 m-0 col-4'
-                ],
-                'attr' => [
-                    'class' => 'my-1 col-7'
-                ]
-            ])
-            ->add('description', TextareaType::class, [
-                'label' => 'Description du livre',
-                'label_attr' => [
-                    'class' => 'font-weight-bold py-1 m-0 col-4'
-                ],
-                'attr' => [
-                    'class' => 'my-1 col-7'
-                ]
-            ])
-            ->add('price', NumberType::class, [
-                'label' => 'Prix du livre',
-                'label_attr' => [
-                    'class' => 'font-weight-bold py-1 m-0 col-4'
-                ],
-                'attr' => [
-                    'class' => 'my-1 col-7'
-                ]
-            ])
-            ->add('stock', NumberType::class, [
-                'label' => 'Nombre de livre en stock',
-                'label_attr' => [
-                    'class' => 'font-weight-bold py-1 m-0 col-4'
-                ],
-                'attr' => [
-                    'class' => 'my-1 col-7'
-                ]
-            ])
-            ->add('year', NumberType::class, [
-                'label' => 'Date de sortie du livre',
-                'label_attr' => [
-                    'class' => 'font-weight-bold py-1 m-0 col-4'
-                ],
-                'attr' => [
-                    'class' => 'my-1 col-7'
-                ]
-            ])
-            ->add('image', TextType::class, [
-                'label' => 'image du livre',
-                'label_attr' => [
-                    'class' => 'font-weight-bold py-1 m-0 col-4'
-                ],
-                'attr' => [
-                    'class' => 'my-1 col-7'
-                ]
-            ])
-            // ->add('submit', SubmitType::class, [
-            //     'label' => 'enregistrer',
-            //     'attr' => [
-            //         'class' => 'btn btn-secondary text-light'
-            //     ]
-            // ])
-            ->getForm();
+        $form = $this->createForm(BookType::class, $book);
 
         $form->handleRequest($request);
 
---
+++ src/Form/BookType.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('title', TextType::class, [
+            'label' => 'Titre du livre',
+            'label_attr' => [
+                'class' => 'font-weight-bold py-1 m-0 col-4'
+            ],
+            'attr' => [
+                'class' => 'my-1 col-7'
+            ]
+        ])
+        ->add('isbn', TextType::class, [
+            'label' => 'Code ISBN du livre',
+            'label_attr' => [
+                'class' => 'font-weight-bold py-1 m-0 col-4'
+            ],
+            'attr' => [
+                'class' => 'my-1 col-7'
+            ]
+        ])
+        ->add('description', TextareaType::class, [
+            'label' => 'Description du livre',
+            'label_attr' => [
+                'class' => 'font-weight-bold py-1 m-0 col-4'
+            ],
+            'attr' => [
+                'class' => 'my-1 col-7'
+            ]
+        ])
+        ->add('price', NumberType::class, [
+            'label' => 'Prix du livre',
+            'label_attr' => [
+                'class' => 'font-weight-bold py-1 m-0 col-4'
+            ],
+            'attr' => [
+                'class' => 'my-1 col-7'
+            ]
+        ])
+        ->add('stock', NumberType::class, [
+            'label' => 'Nombre de livre en stock',
+            'label_attr' => [
+                'class' => 'font-weight-bold py-1 m-0 col-4'
+            ],
+            'attr' => [
+                'class' => 'my-1 col-7'
+            ]
+        ])
+        ->add('year', NumberType::class, [
+            'label' => 'Date de sortie du livre',
+            'label_attr' => [
+                'class' => 'font-weight-bold py-1 m-0 col-4'
+            ],
+            'attr' => [
+                'class' => 'my-1 col-7'
+            ]
+        ])
+        ->add('image', TextType::class, [
+            'label' => 'image du livre',
+            'label_attr' => [
+                'class' => 'font-weight-bold py-1 m-0 col-4'
+            ],
+            'attr' => [
+                'class' => 'my-1 col-7'
+            ]
+        ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
