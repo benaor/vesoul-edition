0bbef1dd30acd48f12f6f41bec8a949b512e9e0b
benaor <benjamingirard25@gmail.com>
2020-06-16 00:41:57 +0200

    create formType for User Registration

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('username')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('phone')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
