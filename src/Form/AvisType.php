893b6dc29c5f95b3934af008af0705cfd1236631
benaor <benjamingirard25@gmail.com>
2020-06-23 13:06:12 +0200

    create AvisType

---
+++ src/Form/AvisType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Avis;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('client')
+            ->add('content')
+            ->add('note')
+            ->add('createdAt')
+            ->add('book')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
