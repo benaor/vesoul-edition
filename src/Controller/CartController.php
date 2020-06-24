10d4d44da226ec068848ed98be114449f11bd611
benaor <benjamingirard25@gmail.com>
2020-06-24 18:22:00 +0200

    create view for cart and function Cart in his controller

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,18 +2,64 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\BookRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/cart")
+ */
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/", name="cart")
      */
-    public function index()
+    public function cart(SessionInterface $session, BookRepository $bookRepo)
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+        $panier = $session->get('panier', []);
+    
+        $panierWithData = [];
+
+        foreach($panier as $id => $quantity){
+            $panierWithData[] = [
+                'book'=> $bookRepo->find($id),
+                'quantity'=> $quantity
+            ];
+        }
+
+        $total = 0;
+        foreach ($panierWithData as $book) {
+            $totalArticle = $book['book']->getPrice()*$book['quantity'];
+            $total += $totalArticle;
+        }
+
+        return $this->render('cart/cart.html.twig', [
+            'controller_name' => 'Votre Panier',
+            'articlesInCart' => $panierWithData,
+            'total'=> $total
         ]);
+
     }
+
+    // /**
+    //  * @Route("/{id}/add", name="cart_add")
+    //  */
+    // public function addToCart()
+    // {
+    // }
+
+    // /**
+    //  * @Route("/{id}/remove", name="cart_remove")
+    //  */
+    // public function removeToCart()
+    // {
+    // }
+
+    // /**
+    //  * @Route("/delete", name="cart_delete")
+    //  */
+    // public function deleteCart()
+    // {
+    // }
 }
